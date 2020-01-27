2a70d669787c41b1cccacff7539cf4c74323769d
jesus <jesushernan3@gmail.com>
2020-01-26 21:31:35 -0300

    prueba de un nivel mas abajo de la estructura para paginas estaticas en php

--- vistas/categoria-css.php
+++ vistas/categoria-css.php
@@ -1,8 +1,7 @@
-
 <?php 
 $raiz="../../";
 include('../estructura/head.php');?>
 
-  contenido de categoria css
+  <a href="<?php echo $raiz?>vistas/css/background-video-css.php">background video</a>
 
 <?php include('../estructura/foot.php');?>
