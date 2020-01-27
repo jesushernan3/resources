194e5b5dd04f89f0b03833aa35f06482fe9db8c7
jesus <jesushernan3@gmail.com>
2020-01-27 20:32:13 -0300

    soluciones estilisticas a la caja de busqueda

--- vistas/categoria-css.php
+++ vistas/categoria-css.php
@@ -1,6 +1,7 @@
 <?php 
-$raiz="../../";
-include('../estructura/head.php');?>
+  $raiz="../../";
+  include('../estructura/head.php');
+?>
 
   <a href="<?php echo $raiz?>vistas/css/background-video-css.php">background video</a>
 
---
+++ vistas/categoria-html.php
@@ -0,0 +1,8 @@
+<?php 
+  $raiz="../../";
+  include('../estructura/head.php');
+?>
+  <p>contenido de categoria html</p>
+  <a href="<?php echo $raiz?>vistas/css/background-video-css.php">background video</a>
+
+<?php include('../estructura/foot.php');?>
